633d2d457181b879d6466ecbd267ba55aab7291b
nocoast <voteforpedro8@gmail.com>
2012-08-08 15:57:47 -0500

    Remove prebuilt kernel and replace with inline kernel build.
    Remove prebuilt kernel as we no longer use it.
    
    Change-Id: I7d1afd887166ebb8008666800e2287da91719624
    
    Revert "Use a prebuild kernel"
    
    This commit broke audio/wifi and additional modules as
    this device calls for kernel modules that manifest in the otapackage
    under lib/modules. kernel/samsung/smdk4412 repo is now needed for build.
    
    This reverts commit cbcbfe6c6d4bc1abf4a87d7b6d1ac711817c6fe6.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -53,12 +53,11 @@ TARGET_RECOVERY_INITRC := device/samsung/i9300/recovery.rc
 TARGET_SPECIFIC_HEADER_PATH := device/samsung/i9300/overlay/include
 
 # Kernel
-#TARGET_KERNEL_SOURCE := kernel/samsung/smdk4412
-#TARGET_KERNEL_CONFIG := cyanogenmod_i9300_defconfig
+TARGET_KERNEL_SOURCE := kernel/samsung/smdk4412
+TARGET_KERNEL_CONFIG := cyanogenmod_i9300_defconfig
 BOARD_KERNEL_CMDLINE := "console=ttySAC2,115200"
 BOARD_KERNEL_BASE := 0x40000000
 BOARD_KERNEL_PAGESIZE := 2048
-TARGET_PREBUILT_KERNEL := device/samsung/i9300/kernel
 
 # Filesystem
 BOARD_NAND_PAGE_SIZE := 4096
