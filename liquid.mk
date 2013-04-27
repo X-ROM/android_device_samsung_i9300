bf36318c4be634d0071ffe6acca658b0ec1fce97
liquid0624 <rjbirnesser@gmail.com>
2013-04-27 15:03:21 -0400

    Move Locale Overrides To Liquid Vendor

--- liquid.mk
+++ liquid.mk
@@ -21,17 +21,11 @@ PRODUCT_RELEASE_NAME := i9300
 # device
 $(call inherit-product-if-exists, device/samsung/i9300/full_i9300.mk)
 
-# languages
-PRODUCT_LOCALES := cs_CZ da_DK de_AT de_CH de_DE de_LI el_GR en_AU en_CA en_GB en_NZ en_SG en_US es_ES fr_CA fr_CH fr_BE fr_FR it_CH it_IT ja_JP ka_GE ko_KR nb_NO nl_BE nl_NL nn_NO pl_PL pt_PT ru_RU sv_SE tr_TR zh_CN zh_HK zh_TW am_ET hi_IN
-
 # gsm
 $(call inherit-product, vendor/liquid/config/common_gsm.mk)
 
 # phone
 $(call inherit-product, vendor/liquid/config/common_phone.mk)
-
-# overlay for international dictionaries
-PRODUCT_PACKAGE_OVERLAYS += vendor/liquid/overlay/all_dictionaries
 
 # products
 PRODUCT_DEVICE := i9300
